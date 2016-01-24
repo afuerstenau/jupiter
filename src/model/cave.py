0fe701d743ec42a1414ce2c447c98d1f0a0c715e
afuerstenau <alex@fuerstenau-clan.de>
2016-01-24 16:34:23 +0100

    refactoring

--- src/model/cave.py
+++ src/model/cave.py
@@ -13,37 +13,34 @@ class Cave:
         self._current_party_field = self._map[0][0]
         self._current_party_field.set_has_char(True)
 
+    def move_party(self):
+        self._current_party_field.set_has_char(False)
+        self._current_party_field =self._map[self._current_party_field_y][self._current_party_field_x]
+        self._current_party_field.set_has_char(True)
+
     def move_party_south(self):
         if not self._current_party_field.is_move_south_possible():
             raise Movement_Not_Possible()
-        self._current_party_field.set_has_char(False)
         self._current_party_field_y += 1
-        self._current_party_field =self._map[self._current_party_field_y][self._current_party_field_x]
-        self._current_party_field.set_has_char(True)
+        self.move_party()
 
     def move_party_north(self):
         if not self._current_party_field.is_move_north_possible():
             raise Movement_Not_Possible()
-        self._current_party_field.set_has_char(False)
         self._current_party_field_y -= 1
-        self._current_party_field =self._map[self._current_party_field_y][self._current_party_field_x]
-        self._current_party_field.set_has_char(True)
+        self.move_party()
 
     def move_party_east(self):
         if not self._current_party_field.is_move_east_possible():
             raise Movement_Not_Possible()
-        self._current_party_field.set_has_char(False)
         self._current_party_field_x += 1
-        self._current_party_field =self._map[self._current_party_field_y][self._current_party_field_x]
-        self._current_party_field.set_has_char(True)
+        self.move_party()
 
     def move_party_west(self):
         if not self._current_party_field.is_move_west_possible():
             raise Movement_Not_Possible()
-        self._current_party_field.set_has_char(False)
         self._current_party_field_x -= 1
-        self._current_party_field =self._map[self._current_party_field_y][self._current_party_field_x]
-        self._current_party_field.set_has_char(True)
+        self.move_party()
 
     def get_map(self):
         return self._map
